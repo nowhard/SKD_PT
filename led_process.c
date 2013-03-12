fb0ab976cd8cbcb8f1370eb003eacb0d6a2b1497
nowhard <nowhard@mail.ru>
2013-03-12 15:41:37 +0300

    Добавлено мигание индикатора, настроен стек

--- led_process.c
+++ led_process.c
@@ -6,37 +6,38 @@
 #include "calibrate/calibrate.h"
 //----------------------------------------------
 
-volatile unsigned char xdata spi_bit_counter;//счетчик передаваемых битов при bit-bang
-volatile unsigned int  xdata spi_data;//данные bit-bang
+volatile unsigned char  spi_bit_counter;//счетчик передаваемых битов при bit-bang
+volatile unsigned int   spi_data;//данные bit-bang
 
-volatile unsigned char xdata spi_delay;//задержка между тактами
-volatile unsigned int  xdata spi_buf[20]={0xC01,0x9FF,0xF00,0xA0E,0xB05,0x505,0x404,0x303,0x208,0x109,0x00,0x00};
-volatile unsigned char xdata spi_buf_length;
-volatile unsigned char xdata spi_buf_counter;
+volatile unsigned char  spi_delay;//задержка между тактами
+volatile unsigned int   spi_buf[20]={0xC01,0x9FF,0xF00,0xA0E,0xB05,0x505,0x404,0x303,0x208,0x109,0x00,0x00};
+volatile unsigned char  spi_buf_length;
+volatile unsigned char  spi_buf_counter;
 
 unsigned int code init_buf[8]={0x100,0x200,0x300,0x400,0x500,0x600,0x700,0x800};
 
-volatile unsigned char xdata num_conv_counter=0;
+volatile unsigned char  num_conv_counter=0;
 
-volatile unsigned long xdata integer_num=0;
+volatile unsigned long  integer_num=0;
 //volatile float         idata float_num=0;
 
 //volatile unsigned char idata indicator_point[current_indicator]=5;//положение точки
 //volatile unsigned char idata MINUS=0;//ставим минус?
 //volatile unsigned char idata BLINK=0;//флаг отображения, используется при мигании
 
-volatile unsigned char xdata flag_blink=0;//позволяет запустить таймер всего один раз
+volatile unsigned char   flag_blink=0;//позволяет запустить таймер всего один раз
 extern volatile struct SKD xdata skd ;
 //----------------------------------------------
-volatile unsigned int indicator_brightness[INDICATOR_NUM]={0xA0F};//,0xA0F,0xA0F,0xA0F};
-volatile unsigned int indicator_scan[INDICATOR_NUM]={0xB04};//,0xB07,0xB04,0xB04};
-volatile unsigned char indicator_point[INDICATOR_NUM]={0x5};//,0x0,0x5,0x5};
-volatile unsigned char indicator_blink[INDICATOR_NUM]={0x0};//,0x0,0x0,0x0};
-volatile unsigned int indicator_decode[INDICATOR_NUM]={0x9FF};//,0x900,0x9FF,0x9FF};
+ unsigned int indicator_brightness[INDICATOR_NUM]={0xA0F};//,0xA0F,0xA0F,0xA0F};
+ unsigned int indicator_scan[INDICATOR_NUM]={0xB04};//,0xB07,0xB04,0xB04};
+ unsigned char indicator_point[INDICATOR_NUM]={0x5};//,0x0,0x5,0x5};
+ unsigned char indicator_blink[INDICATOR_NUM]={0x0};//,0x0,0x0,0x0};
+ unsigned char indicator_blink_flag[INDICATOR_NUM]={0x1};//,0x0,0x0,0x0};
+ unsigned int indicator_decode[INDICATOR_NUM]={0x9FF};//,0x900,0x9FF,0x9FF};
 
-volatile 		  long  indicator_buf[INDICATOR_NUM];//буфер значений индикаторов
+ long  indicator_buf[INDICATOR_NUM];//буфер значений индикаторов
 
-volatile unsigned char current_indicator=INDICATOR_1;//индикатор, захвативший шину
+ unsigned char current_indicator=INDICATOR_1;//индикатор, захвативший шину
 
 //-------------protothreads---------------------
 //unsigned char LED_START=0;
@@ -62,10 +63,7 @@ PT_THREAD(LED_Process(struct pt *pt))
 
   while(1) 
   {
-  		//PT_WAIT_UNTIL(pt,LED_START); //ждем команды на старт
-		//LED_START=0;
-
-	   	//PT_DELAY(pt,1);
+   	    PT_DELAY(pt,2);
 
 		//-----------------предварительная настройка----------
 		 spi_buf[1]=indicator_decode[current_indicator];//устанавливаем decode mode
@@ -105,22 +103,25 @@ PT_THREAD(LED_Process(struct pt *pt))
 				   }
 					
 				}
-
 			}
 			//----------------------------поставим минус-------------------------------------			
 			if(indicator_buf[current_indicator]<0)  //по возможности автоматизировать подстройку debug
 			{
 				spi_buf[9]=(spi_buf[9]&0xFFF0)|0xA;//ставим минус, если число отрицательное
 			}
-//				//-------------------------------------------------------------------------------
-//				if(indicator_blink[current_indicator])
-//				{
-//					for(i=0;i<(indicator_scan[current_indicator]+1);i++)
-//					{
-//						 spi_buf[5+i]&=0xff00;
-//						 spi_buf[5+i]|=0x000f;
-//					}
-//				}
+			//-------------------------------------------------------------------------------
+			if(indicator_blink[current_indicator])
+			{
+				if(indicator_blink_flag[current_indicator]==0)
+				{
+					for(i=0;i<((indicator_scan[current_indicator]&0x7)+1);i++)
+					{
+						 spi_buf[5+i]&=0xff00;
+						 spi_buf[5+i]|=0x000f;
+					}
+				}
+			}
+			//-------------------------------------------------------------------------------
 		}
 		else//SET BARGRAPH
 		{
@@ -290,50 +291,6 @@ void LED_Out_Float(unsigned char  display_num,float  num)//вывод значения с плав
 	return;
 }
 //--------------------------------------------------------------------------------
-//void LED_Out_Integer(unsigned long num)//вывод целочисленного значения
-//{
-////	if(spi_auto==SPI_WAIT)
-////	{
-//		integer_num=num;//...
-//		//spi_auto=SPI_INT_TO_BCD;
-//		LED_START=1;//стартуем поток
-//
-//		if(flag_blink==0)
-//		{
-//			LED_Set_Brightness(skd.brightness>>4);
-//		}
-//		else
-//		{
-//			LED_Set_Brightness(0xf);
-//		}	
-////	}
-//	return;
-//}
-//--------------------------------------------------------------------------------
-//void LED_Out_Buf(unsigned int out_buf[], unsigned char out_buf_len)//вывод буфера в SPI bit-bang
-//{
-//	unsigned char i;
-//
-////	if(spi_auto!=SPI_WAIT)
-////	{
-//	//	return;
-//	//}
-//	//скопируем в свой буфер
-//	for(i=0;i<(out_buf_len&0xF);i++)
-//	{
-//		spi_buf[i]=out_buf[i];	
-//	}
-//	
-//	spi_buf_counter=0;
-//	spi_data=spi_buf[spi_buf_counter];
-//	spi_buf_counter++;
-//	spi_buf_length=out_buf_len;
-//	CS=0;
-////	spi_auto=SPI_SEND_WORD;
-////	sw_auto=0;
-//	return;	
-//}
-//--------------------------------------------------------------------------------
 void LED_Set_Digit_Num(unsigned char display_num,unsigned char num)//установить количество отображаемых чисел
 {
 	//(spi_buf[4]&0xfff0)|((num-1)&0x7);
@@ -372,7 +329,7 @@ void LED_SetBlink(unsigned char display_num,unsigned char blink)//мигание
 
 PT_THREAD(LED_BlinkTask(struct pt *pt))//процесс мигания
  {
- 	unsigned char i=0;
+ 	static    unsigned char i=0;
   PT_BEGIN(pt);
 
     while(1) 
@@ -380,7 +337,7 @@ PT_THREAD(LED_BlinkTask(struct pt *pt))//процесс мигания
 	   PT_DELAY(pt,100);
 	   for(i=0;i<INDICATOR_NUM;i++)
 	   {
-	   		indicator_blink[i]=!indicator_blink[i];	
+	   		indicator_blink_flag[i]^=1;	
 	   }
 	}
   PT_END(pt);
